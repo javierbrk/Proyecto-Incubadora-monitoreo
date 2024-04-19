d8f82662fda7ae3b8a2126e54f53ae8f444ebf53
Annie <anyscigliano@gmail.com>
2024-04-19 15:59:19 -0300

    copied lib folder

--- src/mobile_app/lib/utils/constants.dart
+++ src/mobile_app/lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'http://192.168.1.6';
+  static String baseUrl = 'http://127.0.0.1:5000';
   static String maxTempEndPoint = '/maxtemp';
   static String minTempEndPoint = '/mintemp';
   static String versionEndPoint = '/version';

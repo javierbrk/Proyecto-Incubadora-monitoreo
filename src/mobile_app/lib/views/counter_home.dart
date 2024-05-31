e0757daf0511703626dc99e2b1c3d54b552ade73
Annie <anyscigliano@gmail.com>
2024-05-30 21:27:51 -0300

    configurations view shows the elapsed time since the tray buttons were pressed

--- src/mobile_app/lib/views/counter_home.dart
+++ src/mobile_app/lib/views/counter_home.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:collection'; 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:incubapp_lite/views/home.dart';
 import 'package:incubapp_lite/views/wifi_home.dart';
 import 'package:incubapp_lite/views/initial_home.dart';
-// import 'package:incubapp_lite/services/api_services.dart';
+import 'package:incubapp_lite/services/api_services.dart';
 // import 'package:incubapp_lite/services/counter_home.dart';
 import 'package:incubapp_lite/views/graf_home.dart';
+import 'package:incubapp_lite/models/config_model.dart';
+
 
 class CHome extends StatefulWidget {
   @override
@@ -19,8 +22,35 @@ class _CHomeState extends State<CHome> {
 
   List<int> _bandejaSelectedIndexList = [1, 1, 1]; 
 
+  List<int?> _bandejaTimestamps = [0, 0, 0];
+
+  late Config? _configModel = Config(ssid: "SSID", minTemperature: 37, maxTemperature: 39, rotationPeriod: 3600000, rotationDuration: 5000, passwd: "12345678", hash: 1234, incubationPeriod: 18, trayOneDate: 10000, trayTwoDate: 5000, trayThreeDate: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+  Future<void> _getData() async {
+    _configModel = await ApiService().getConfig();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    
+    int hash = _configModel?.hash ?? 0;
+    int incubation_period = _configModel?.incubationPeriod ?? 0;
+    int maxtemp = _configModel?.maxTemperature ?? 0;
+    int mintemp = _configModel?.minTemperature ?? 0;
+    String password = _configModel?.passwd ?? "";
+    int rotation_duration = _configModel?.rotationDuration ?? 0;
+    int rotation_period =_configModel?.rotationPeriod ?? 0;
+    String ssid = _configModel?.ssid ?? "";
+    int tray_one_date = _configModel?.trayOneDate ?? 0;
+    int tray_two_date = _configModel?.trayTwoDate ?? 0;
+    int tray_three_date = _configModel?.trayThreeDate ?? 0;
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Contador de Días'),
@@ -38,7 +68,14 @@ class _CHomeState extends State<CHome> {
                       _showRedDialog(0);
                     } else {
                       _bandejaSelectedIndexList[0] = 0;
-                      _showGreenDialog();
+                      _showGreenDialog(0);
+                      _bandejaTimestamps[0] = DateTime.now().millisecondsSinceEpoch;
+                      int? newTrayOneDate = _bandejaTimestamps[0];
+                      setState(() {
+                                _configModel?.trayOneDate = newTrayOneDate!;
+                              });
+                              ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': password,'rotation_duration': rotation_duration,'rotation_period': rotation_period,'ssid': ssid,'tray_one_date': newTrayOneDate,'tray_three_date': _configModel?.trayThreeDate,'tray_two_date': _configModel?.trayTwoDate});
+                      print('Timestamp: ${_bandejaTimestamps[0]}');
                     }
                   });
                   print('BANDEJA 1 pressed!');
@@ -62,10 +99,17 @@ class _CHomeState extends State<CHome> {
                       _showRedDialog(1);
                     } else {
                       _bandejaSelectedIndexList[1] = 0;
-                      _showGreenDialog();
+                      _showGreenDialog(1);
+                      _bandejaTimestamps[1] = DateTime.now().millisecondsSinceEpoch;
+                      int? newTrayTwoDate = _bandejaTimestamps[1];
+                      setState(() {
+                                _configModel?.trayTwoDate = newTrayTwoDate!;
+                              });
+                              ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': password,'rotation_duration': rotation_duration,'rotation_period': rotation_period,'ssid': ssid,'tray_one_date': _configModel?.trayOneDate,'tray_three_date': _configModel?.trayThreeDate,'tray_two_date': newTrayTwoDate});
+                      print('Timestamp: ${_bandejaTimestamps[1]}');
                     }
                   });
-                  print('BANDEJA 2 pressed!');
+                  print('BANDEJA 2 pressed!');                  
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: _bandejaSelectedIndexList[1] == 1 ? const Color.fromARGB(255, 138, 201, 140) :  const Color.fromARGB(255, 179, 65, 65),
@@ -86,10 +130,17 @@ class _CHomeState extends State<CHome> {
                       _showRedDialog(2);
                     } else {
                       _bandejaSelectedIndexList[2] = 0;
-                      _showGreenDialog();
+                      _showGreenDialog(2);
+                      _bandejaTimestamps[2] = DateTime.now().millisecondsSinceEpoch;
+                      int? newTrayThreeDate = _bandejaTimestamps[2];
+                      setState(() {
+                                _configModel?.trayThreeDate = newTrayThreeDate!;
+                              });
+                              ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': password,'rotation_duration': rotation_duration,'rotation_period': rotation_period,'ssid': ssid,'tray_one_date': _configModel?.trayOneDate,'tray_three_date': newTrayThreeDate,'tray_two_date': _configModel?.trayTwoDate});
+                      print('Timestamp: ${_bandejaTimestamps[2]}');
                     }
                   });
-                  print('BANDEJA 3 pressed!');
+                  print('BANDEJA 3 pressed!');                  
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: _bandejaSelectedIndexList[2] == 1 ? const Color.fromARGB(255, 138, 201, 140) : const Color.fromARGB(255, 179, 65, 65),
@@ -159,7 +210,7 @@ class _CHomeState extends State<CHome> {
     }
   }
 
-  void _showGreenDialog() {
+  void _showGreenDialog(int index) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -195,7 +246,25 @@ class _CHomeState extends State<CHome> {
             onPressed: () {
               setState(() {
                 _bandejaSelectedIndexList[index] = 1; // Cambiar el color del botón
+                _bandejaTimestamps[index] = 0;
+                print('Timestamp: ${_bandejaTimestamps[index]}');
               });
+
+              switch (index) {
+                case 0:
+                  _configModel?.trayOneDate = _bandejaTimestamps[index]!;
+                  ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': _configModel?.passwd,'rotation_duration': _configModel?.rotationDuration,'rotation_period': _configModel?.rotationPeriod,'ssid': _configModel?.ssid,'tray_one_date': _configModel?.trayOneDate,'tray_three_date': _configModel?.trayThreeDate,'tray_two_date': _configModel?.trayTwoDate});
+                  break;
+                case 1:
+                  _configModel?.trayTwoDate = _bandejaTimestamps[index]!;
+                  ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': _configModel?.passwd,'rotation_duration': _configModel?.rotationDuration,'rotation_period': _configModel?.rotationPeriod,'ssid': _configModel?.ssid,'tray_one_date': _configModel?.trayOneDate,'tray_three_date': _configModel?.trayThreeDate,'tray_two_date': _configModel?.trayTwoDate});
+                  break;
+                case 2:
+                  _configModel?.trayThreeDate = _bandejaTimestamps[index]!;
+                  ApiService().updateConfig({'hash': _configModel?.hash,'incubation_period': _configModel?.incubationPeriod,'max_temperature': _configModel?.maxTemperature,'min_temperature': _configModel?.minTemperature,'passwd': _configModel?.passwd,'rotation_duration': _configModel?.rotationDuration,'rotation_period': _configModel?.rotationPeriod,'ssid': _configModel?.ssid,'tray_one_date': _configModel?.trayOneDate,'tray_three_date': _configModel?.trayThreeDate,'tray_two_date': _configModel?.trayTwoDate});
+                  break;
+              }
+
               Navigator.of(context).pop(); // Cerrar el diálogo
             },
             child: Text('Aceptar'),

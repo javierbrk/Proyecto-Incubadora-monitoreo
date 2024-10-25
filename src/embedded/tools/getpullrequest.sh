67f4bd124584909ea089f9fa40828ebcef61f557
santiagocetran <santiagorcetran236@gmail.com>
2024-10-25 11:58:59 -0300

    pull request and reomte repo script

--- src/embedded/tools/getpullrequest.sh
+++ src/embedded/tools/getpullrequest.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+REMOTE_NAME="am_remote"
+REMOTE_URL="https://github.com/AlterMundi-MonitoreoyControl/Proyecto-Incubadora.git"
+
+# Check if the remote is already configured
+if git remote get-url "$REMOTE_NAME" &> /dev/null; then
+    echo "Remote '$REMOTE_NAME' is already configured."
+else
+    echo "Remote '$REMOTE_NAME' is not configured. Adding it now..."
+    git remote add "$REMOTE_NAME" "$REMOTE_URL"
+    echo "Remote '$REMOTE_NAME' added with URL: $REMOTE_URL"
+fi 
+
 echo "obteniendo pull numero "$1
 git status 
 echo "verificar que no hay cambios si ves cambios presionar ctrl-c" 
 read -n 1 -s
-git fetch annie pull/$1/head:$1
+git fetch am_remote pull/$1/head:$1
 git checkout $1
 #$appfilecopy
 echo "seguir procedimiento de pruebas para verificar que no se rompio nada"

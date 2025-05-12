4fb9952e7e646ff01cf6673ea0e66b030701e8c2
Javier <javierbrk@users.noreply.github.com>
2025-05-12 11:53:18 -0300

    Update credentials.lua

--- src/embedded/libs/credentials.lua
+++ src/embedded/libs/credentials.lua
@@ -47,3 +47,5 @@ gpio.write(GPIOVOLTEO_EN, 0)
 gpio.config( { gpio={GPIOHUMID}, dir=gpio.OUT })
 gpio.set_drive(GPIOHUMID, gpio.DRIVE_3)
 gpio.write(GPIOHUMID, 1)
+
+ gpio = { GPIOREEDS_DOWN, GPIOREEDS_UP }, dir = gpio.IN, pull = gpio.PULL_UP })
